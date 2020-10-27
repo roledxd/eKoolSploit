3e87fad195f0f83a161797c142b46f707d5902f6
RoLedXD <44301044+roledxd@users.noreply.github.com>
2020-10-27 21:04:29 +0200

    Update gmainv2ph_NEWMOD.js

--- mods/gmainv2ph_NEWMOD.js
+++ mods/gmainv2ph_NEWMOD.js
@@ -1,2 +1,12 @@
-guserPH.userAccountTypes = function(){return [1, 2, 3, 4, 5, 6, 7, 10, 11, 20, 21, 22, 23];}
-guserPH.getAccountType = function(){return [1, 2, 3, 4, 5, 6, 7, 10, 11, 20, 21, 22, 23];}
+var allroles = [1, 2, 3, 4, 5, 6, 7, 10, 11, 20, 21, 22, 23];
+guserPH.userAccountTypes = function(){return allroles;}
+guserPH.getAccountType = function(){return allroles;}
+guserPH.isStaffRole = function(){return true;}
+guserPH.isFormMaster = function(){return true;}
+guserPH.isHeadmaster = function(){return true;}
+guserPH.getRoles = function(){[allroles]}
+guserPH.getActiveTeacherRoles = function(){return [gmainv2PH.groupObj]}
+guserPH.getActiveStaffRoles = function(){return [gmainv2PH.groupObj]}
+/*if(wpRep){
+  guserPH.getMasteredClasses = function(){return wpRep.masteredClasses;}
+}*/
