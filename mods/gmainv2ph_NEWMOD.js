dcf1da7314b4a528c865eb9d9ebd5e9efb266625
RoLedXD <44301044+roledxd@users.noreply.github.com>
2020-11-25 20:51:24 +0200

    Update gmainv2ph_NEWMOD.js

--- mods/gmainv2ph_NEWMOD.js
+++ mods/gmainv2ph_NEWMOD.js
@@ -2,9 +2,17 @@ var u = guserPH.user;
 var allroles = [1, 2, 3, 4, 5, 6, 7, 10, 11, 20, 21, 22, 23];
 var srr = new ee.ekool.model.roles.Staff;
 srr.id = u.id;
+srr.person = u.person;
+srr.institution = u.institution;
+srr.instanceClass = u.instanceClass;
 var trr = new ee.ekool.model.roles.Teacher;
 trr.id = u.id;
+trr.person = u.person;
+trr.institution = u.institution;
+trr.instanceClass = u.instanceClass;
 guserPH.userAccountTypes = function(){return allroles;}
+guserPH.user.allAvailableRoles.push(srr);
+guserPH.user.allAvailableRoles.push(trr);
 guserPH.getAccountType = function(){return ROLES.ADMIN}
 guserPH.isStaffRole = function(){return true;}
 guserPH.isFormMaster = function(){return true;}
@@ -12,6 +20,8 @@ guserPH.isHeadmaster = function(){return true;}
 guserPH.getRoles = function(){return allroles}
 guserPH.getActiveTeacherRoles = function(){return [trr]}
 guserPH.getActiveStaffRoles = function(){return [srr]}
+//rolesPH.isAdmin = true;
+//rolesPH.accountTypes = allroles;
 /*if(wpRep){
   guserPH.getMasteredClasses = function(){return wpRep.masteredClasses;}
 }*/
