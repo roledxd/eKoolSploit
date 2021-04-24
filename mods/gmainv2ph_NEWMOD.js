f1aa3be7720d6edbca61925bd2db7f009068f04c
RoLedXD <44301044+roledxd@users.noreply.github.com>
2021-04-24 19:58:35 +0300

    Update gmainv2ph_NEWMOD.js

--- mods/gmainv2ph_NEWMOD.js
+++ mods/gmainv2ph_NEWMOD.js
@@ -20,6 +20,11 @@ guserPH.isHeadmaster = function(){return true;}
 guserPH.getRoles = function(){return allroles}
 guserPH.getActiveTeacherRoles = function(){return [trr]}
 guserPH.getActiveStaffRoles = function(){return [srr]}
+gmainv2PH.getMenuItemsPermissions = function() {
+    var a = {"requests": true,"timetable": true"timetableAdmin": true,"reports": true,"fa": true,"recordBook": true,"interview": true,"interviewDemo": true,"descisions": true,"schoolyear": true,"recources": true,"curriculum": true,"gradesTransfer": true,"classificators": true,"addmission": true,"discipline": true,"fullDiscipline": true,"gradesTransferAdmin": true,"superAdminTool": true,"ads": true,"cafeteria": true,"devCardsTemplates": true,};
+    gmainv2PH.permissions = a;
+    return a;
+}
 //rolesPH.isAdmin = true;
 //rolesPH.accountTypes = allroles;
 /*if(wpRep){
