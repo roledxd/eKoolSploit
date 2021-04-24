206d42b791366d84360d4710df5972f8eaec9d89
RoLedXD <44301044+roledxd@users.noreply.github.com>
2021-04-24 20:02:16 +0300

    Update adminRights.js

--- mods/adminRights.js
+++ mods/adminRights.js
@@ -21,7 +21,7 @@ guserPH.getRoles = function(){return allroles}
 guserPH.getActiveTeacherRoles = function(){return [trr]}
 guserPH.getActiveStaffRoles = function(){return [srr]}
 gmainv2PH.getMenuItemsPermissions = function() {
-    var a = {"requests": true,"timetable", true:"timetableAdmin": true,"reports": true,"fa": true,"recordBook": true,"interview": true,"interviewDemo": true,"descisions": true,"schoolyear": true,"recources": true,"curriculum": true,"gradesTransfer": true,"classificators": true,"addmission": true,"discipline": true,"fullDiscipline": true,"gradesTransferAdmin": true,"superAdminTool": true,"ads": true,"cafeteria": true,"devCardsTemplates": true,};
+    var a = {"requests": true,"timetable": true,"timetableAdmin": true,"reports": true,"fa": true,"recordBook": true,"interview": true,"interviewDemo": true,"descisions": true,"schoolyear": true,"recources": true,"curriculum": true,"gradesTransfer": true,"classificators": true,"addmission": true,"discipline": true,"fullDiscipline": true,"gradesTransferAdmin": true,"superAdminTool": true,"ads": true,"cafeteria": true,"devCardsTemplates": true,};
     gmainv2PH.permissions = a;
     return a;
 }
